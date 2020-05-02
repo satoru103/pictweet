395feb53d41a338b48a0bc71ff7d815de5ad868e
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-05-02 16:11:14 +0900

    投稿削除機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,15 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+  @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+      redirect_to root_path
+    else
+      render @tweet
+    end
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name,:text,:image)
