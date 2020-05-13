57a855873dedfdebea118443440d8d27c2c0dfea
satoru103 <58040354+satoru103@users.noreply.github.com>
2020-05-13 21:16:32 +0900

    検索機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit,:show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show,:search]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
@@ -34,6 +34,10 @@ class TweetsController < ApplicationController
     else
       render @tweet
     end
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
 
